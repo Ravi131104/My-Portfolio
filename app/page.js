d14bd90ce32b1ede170abf0bb28020956a168127
Ravi131104 <145458454+Ravi131104@users.noreply.github.com>
2025-01-28 20:06:46 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 "use client"
-import Navbar from "@/components/Navbar";
-import Header from "@/components/Header";
-import About from "@/components/About";
-import Services from "@/components/Services";
-import Work from "@/components/work";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import About from "../components/About";
+import Services from "../components/Services";
+import Work from "../components/work";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 import { useEffect, useState } from "react";
 export default function Home() {
   
