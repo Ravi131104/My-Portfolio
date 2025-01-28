c2301d6ced3394dd6ce688793579537e98e6c969
Ravi131104 <145458454+Ravi131104@users.noreply.github.com>
2025-01-28 21:35:11 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import About from "../components/About";
 import Services from "../components/Services";
-import Work from "../components/work";
+import Work from "../components/Work";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import { useEffect, useState } from "react";
